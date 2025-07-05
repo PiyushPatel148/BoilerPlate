e5a70796cf857132442eac528930442bf7fa65f7
Piyush Patel <piyushpatel@adobe.com>
2025-07-05 17:56:52 +0530

    hero block initial

---
+++ blocks/test/test.js
@@ -0,0 +1,11 @@
+export default  function decorate(block){
+   [...block.children].forEach((row) => {
+    let iter =0;
+    [...row.children].forEach((col) => {
+        if(col.querySelector('picture')){
+            const picture = col.querySelector('picture');
+            picture.classList.add(`hero-image-${iter++}`);
+        }
+    });
+});
+}
