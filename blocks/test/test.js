2328aa6ffe078f865c7e64c46fb1ab37f539e39f
Piyush Patel <piyushpatel@adobe.com>
2025-07-05 18:12:49 +0530

    linting fixes

--- blocks/test/test.js
+++ blocks/test/test.js
@@ -1,11 +1,12 @@
-export default  function decorate(block){
-   [...block.children].forEach((row) => {
-    let iter =0;
+export default function decorate(block) {
+  [...block.children].forEach((row) => {
+    let iter = 0;
     [...row.children].forEach((col) => {
-        if(col.querySelector('picture')){
-            const picture = col.querySelector('picture');
-            picture.classList.add(`hero-image-${iter++}`);
-        }
+      if (col.querySelector('picture')) {
+        const picture = col.querySelector('picture');
+        picture.classList.add(`hero-image-${iter}`);
+        iter += 1;
+      }
     });
-});
+  });
 }
