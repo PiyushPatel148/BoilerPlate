624ee54de4bd7e6ba9cbc7a7f1bb85dfd3bcdbc5
Piyush Patel <piyushpatel@adobe.com>
2025-07-15 01:50:37 +0530

    final update to hero-section

--- blocks/hero-section/hero-section.js
+++ blocks/hero-section/hero-section.js
@@ -12,4 +12,27 @@ export default function decorate(block) {
       }
     });
   });
+
+  const mediaQuery = window.matchMedia('(max-width: 768px)');
+
+  function handleViewportChange(e) {
+    if (e.matches) {
+      const img = block.querySelector('.hero-image-0');
+      img.style.display = 'none';
+      // Screen is 480px or less
+
+    // document.body.classList.add('mobile');
+    } else {
+    // Screen is wider than 480px
+      const img = block.querySelector('.hero-image-0');
+      img.style.display = 'block';
+      document.body.classList.remove('mobile');
+    }
+  }
+
+  // Initial check
+  handleViewportChange(mediaQuery);
+
+  // Listen for changes
+  mediaQuery.addEventListener('change', handleViewportChange);
 }
