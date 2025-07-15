febb93eccb4a1bfc31491604da2805879611ef97
Piyush Patel <piyushpatel@adobe.com>
2025-07-16 01:01:54 +0530

    fixed linting errors

--- blocks/success-screen/success-screen.js
+++ blocks/success-screen/success-screen.js
@@ -2,14 +2,14 @@ export default async function decorate(block) {
   const container = block.closest('.success-screen-container') || block;
 
   // Hide the container initially
-  container.style.setProperty('display', 'none', 'important');
+  container.style.setProperty('display', 'none');
 
   function showSuccessScreen() {
     // Prevent background scroll
     document.body.classList.add('success-modal-open');
-    container.style.setProperty('display', 'flex', 'important');
+    container.style.setProperty('display', 'flex');
     setTimeout(() => {
-      container.style.setProperty('display', 'none', 'important');
+      container.style.setProperty('display', 'none');
       document.body.classList.remove('success-modal-open');
     }, 3000);
   }
